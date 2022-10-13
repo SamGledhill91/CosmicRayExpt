f8d86ddff63a296942d92952d6b41fc765d4aae7
Zackmuons <115149021+Zackmuons@users.noreply.github.com>
2022-10-13 11:39:49 +0100

    Update threshanalysis.py

--- analysis/threshanalysis.py
+++ analysis/threshanalysis.py
@@ -1,1 +1,49 @@
 
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+from event import Event, Pulse
+
+
+print("Starting analysis")
+
+import sys
+def function_name(filestoprocess): 
+  
+  j = []
+  
+  for input in filestoprocess:
+    print "Processing %s" % input
+    count = 0
+    with open(input,'rb') as afile:
+      events = pickle.load(afile)
+      for event in events:
+        for pulse in eventy.pulses:
+          if pulse.edge == 0:
+            count += 1
+      i = (input,count)
+      j.append(i)
+    return j
+function_name(sys.argv[1:])
+
+print(j)
+
+
+# open the file
+ifile = open(args.in_file, 'rb')
+events= pickle.load(ifile)
+n_events= len(events)
+
+print("Read {} events from file".format(n_events))
+
+# example event loop
+count = [0, 0, 0, 0]  # counts per channel
+
+for event in events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count[pulse.chan] += 1
+
