0ae893119cfe10cfdc0c61b2c15489fd4d504fc2
Zackmuons <115149021+Zackmuons@users.noreply.github.com>
2022-10-14 13:06:49 +0100

    Update threshanalysis.py

--- analysis/threshanalysis.py
+++ analysis/threshanalysis.py
@@ -11,7 +11,6 @@ from event import Event, Pulse
 pathToFiles = '/home/ph18493/data/threshcali'
 threshIncrement =  100
 
-matrix
 
 thresholds=["100","200","300","400","500","600","700","800","900","1000"]
 threshs = [int(x) for x in thresholds]
@@ -25,21 +24,30 @@ channel_tube_dict={"pmt1":3,
 		   "pmt8":0}
 
 for tube in channel_tube_dict.keys():
-    counts=[]
+	counts=[]
 
-    for thres in thresholds:
-        filename="thresh%s%s.pkl"%(thres,tube)
-        ifile = open(filename, 'rb')
-        events = pickle.load(ifile)
-        count=0
-        for event in events:
-            for pulse in event.pulses:
-                # only count rising edges
-                if pulse.edge == 0:
-                    count+=1
-        counts.append(count)
+    	for thres in thresholds:
+        	filename="thresh%s%s.pkl"%(thres,tube)
+        	ifile = open(filename, 'rb')
+        	events = pickle.load(ifile)
+        	count=0
+        	for event in events:
+            	for pulse in event.pulses:
+                	# only count rising edges
+                	if pulse.edge == 0:
+                    		count+=1
+        	counts.append(count)
 
-    plt.plot(threshs,counts)
+    	
+	plt.plot(threshs,counts, label=tube)
+	
+
+plt.xlabel("Threshhold mV")
+plt.ylabel("Counts")
+
+plt.legend()
+
+plt.show()
      
 
 
