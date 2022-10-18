8819aee7c3bb96ae8cfe90d5dfe63f3b07623c4c
SamGledhill91 <115148782+SamGledhill91@users.noreply.github.com>
2022-10-18 11:49:12 +0100

    Update looprun.sh

--- DAQ/looprun.sh
+++ DAQ/looprun.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 for j in 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 575 600; do
-  acquire.py -t 100 -o thresh$jpmt1.pkl -0 $j -1 $j -2 $j -3 -e 0x8 
-  acquire.py -t 100 -o thresh$jpmt2.pkl -0 $j -1 $j -2 $j -3 -e 0x4
-  acquire.py -t 100 -o thresh$jpmt3.pkl -0 $j -1 $j -2 $j -3 -e 0x2
-  acquire.py -t 100 -o thresh$jpmt4.pkl -0 $j -1 $j -2 $j -3 -e 0x1
+  acquire.py -t 100 -o thresh$jpmt8.pkl -0 $j -1 $j -2 $j -3 -e 0x8 
+  acquire.py -t 100 -o thresh$jpmt7.pkl -0 $j -1 $j -2 $j -3 -e 0x4
+  acquire.py -t 100 -o thresh$jpmt6.pkl -0 $j -1 $j -2 $j -3 -e 0x2
+  acquire.py -t 100 -o thresh$jpmt5.pkl -0 $j -1 $j -2 $j -3 -e 0x1
 done
