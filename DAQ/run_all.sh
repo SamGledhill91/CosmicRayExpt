b9af69ef795e6be5cb122aba198bd9a4d76bb1c3
SamGledhill91 <115148782+SamGledhill91@users.noreply.github.com>
2022-11-07 16:20:12 +0000

    Update run_all.sh

--- DAQ/run_all.sh
+++ DAQ/run_all.sh
@@ -10,4 +10,4 @@ acquire.py -t 3600 -c 2 -o 1hrcoinc458.txt -0 300 -1 400 -2 400 -3 275 -e 0x7
 acquire.py -t 3600 -c 2 -o 1hrcoinc158.txt -0 300 -1 400 -2 400 -3 275 -e 0xB
 acquire.py -t 3600 -c 2 -o 1hrcoinc148.txt -0 300 -1 400 -2 400 -3 275 -e 0xD
 acquire.py -t 3600 -c 2 -o 1hrcoinc145.txt -0 300 -1 400 -2 400 -3 275 -e 0xE
-acquire.py -t 3600 -c 3 -o 10hrcoinc1458.txt -0 300 -1 400 -2 400 -3 275 -e 0xF
+acquire.py -t 36000 -c 3 -o 10hrcoinc1458.txt -0 300 -1 400 -2 400 -3 275 -e 0xF
